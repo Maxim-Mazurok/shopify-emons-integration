7236ece7dc144f70262a3871a9fe55f99a436bbc
Maxim Mazurok <maxim@mazurok.com>
2019-05-19 15:03:07 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Requirements
+- NodeJS (tested on v10.15.3, see `engines` section of `package.json`)
+- MySQL (tested on v5.7.18)
---
+++ index.js
@@ -0,0 +1,40 @@
+const ENV = process.env.DEV === "true" ? '.env.local' : '.env';
+const path = require('path');
+require('dotenv').config({ path: path.resolve(process.cwd(), ENV) });
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS, {
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
+    dialect: 'mysql',
+  });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+//
+// class User extends Sequelize.Model {
+// }
+//
+// User.init({
+//   username: Sequelize.STRING,
+//   birthday: Sequelize.DATE
+// }, { sequelize, modelName: 'user' });
+//
+// sequelize.sync()
+//   .then(() => User.create({
+//     username: 'janedoe',
+//     birthday: new Date(1980, 6, 20)
+//   }))
+//   .then(jane => {
+//     console.log(jane.toJSON());
+//   });
