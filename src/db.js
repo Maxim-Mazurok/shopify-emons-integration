be5e719c17eeeb284a6c244db053762f081e3df3
Maxim Mazurok <maxim@mazurok.com>
2019-07-01 20:47:38 +0300

    fix

--- src/db.js
+++ src/db.js
@@ -42,21 +42,3 @@ const addOrder = json => {
 module.exports = {
   addOrder
 };
-
-//
-// class User extends Sequelize.Model {
-// }
-//
-// User.init({
-//   username: Sequelize.STRING,
-//   birthday: Sequelize.DATE
-// }, { sequelize, modelName: 'user' });
-//
-// sequelize.sync()
-//   .then(() => User.create({
-//     username: 'janedoe',
-//     birthday: new Date(1980, 6, 20)
-//   }))
-//   .then(jane => {
-//     console.log(jane.toJSON());
-//   });
